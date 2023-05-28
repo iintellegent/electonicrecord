9dce9755d34161bace8b53f37e56c9e5337a0ee6
Жакашева Аделина <adelya.zha03@gmail.com>
2023-05-28 17:30:36 +0300

    смена пароля

--- electronicrecord/electronicrecord/settings.py
+++ electronicrecord/electronicrecord/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'electronicrecordConnection',
         'USER':  'root',
-        'PASSWORD':  '12345678',
+        'PASSWORD':  '123Qwerty!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
