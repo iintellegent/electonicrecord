220c81b3c03688f2778ccf7f5a794b8fbf9208ef
Петрова Елизавета <iintellegent03@mail.ru>
2023-05-28 17:14:10 +0300

    доделали миграцию

--- electronicrecord/recordapp/views.py
+++ electronicrecord/recordapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
-
+from rest_framework.response import Response
 # Create your views here.
+from rest_framework.decorators import api_view
+from .models import *
+from .serializer import *
+@api_view(['GET'])
+def getClient(request):
+
+    client = Client.objects.all()
+
+    srlz = ClientSerializer(client, many=True)
+
+    return Response(srlz.data)
+
