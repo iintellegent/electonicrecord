b5434e75e392ef10033dc8840dd13734bc03bb6e
Петрова Елизавета <iintellegent03@mail.ru>
2023-06-07 18:17:58 +0300

    доделали Client, Specialist

--- electronicrecord/recordapp/urls.py
+++ electronicrecord/recordapp/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('client/', views.getClient)
+    path('client/', views.getClient),
+    path('client/add', views.add_client),
+    path('specialist/', views.get_specialist),
+    path('specialist/add', views.add_specialist)
  ]
